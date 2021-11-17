89c401a0085a8f9d1f63d5968997efe5bc98736a
unknown <you@example.com>
2021-11-18 01:57:58 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const app = express()
+const port = 5000
+const { MongoClient } = require('mongodb');
+const ObjectId = require("mongodb").ObjectId;
+const cors = require("cors")
+
+// MiddleWare
+app.use(cors());
+app.use(express.json())
+
+const uri = "mongodb+srv://electron:pRNt5auNBsMQxVGg@cluster0.tlrw7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+
+
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("Repiring-shop");
+        const productsCollection = database.collection("products");
+        // create a document to insert
+
+    } finally {
+        await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
